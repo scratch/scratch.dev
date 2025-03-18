83f255f8f4408d461043b800cb75bb6b64ae2123
Peter Koomen <koomen@gmail.com>
2025-03-17 18:00:58 -0700

    clean up page wrapper

--- website/components/PageWrapper.tsx
+++ website/components/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import Navigation from "./Navigation";
+import React from "react";
 
 export default function PageWrapper({
   children,
@@ -6,9 +6,12 @@ export default function PageWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Navigation />
-      <div className="container mx-auto p-4">{children}</div>
-    </>
+    <div className="min-h-screen bg-gray-50 py-8 font-mono">
+      <div className="container mx-auto max-w-4xl px-4">
+        <div className="rounded-lg bg-white p-6 shadow-sm">
+          {children}
+        </div>
+      </div>
+    </div>
   );
 }
