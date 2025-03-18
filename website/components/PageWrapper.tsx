554fec36f3684467d7d1937dc8593512df7a7217
Peter Koomen <koomen@gmail.com>
2025-03-17 21:59:03 -0700

    allow for editing of the page title

--- website/components/PageWrapper.tsx
+++ website/components/PageWrapper.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 export default function PageWrapper({
   children,
+  title,
 }: {
   children: React.ReactNode;
+  title?: string;
 }) {
   return (
-    <div className="min-h-screen bg-white font-sans">
-      <div className="container mx-auto max-w-4xl px-4 py-8">
-        {children}
+    <>
+      <Helmet>
+        {title && <title>{title}</title>}
+      </Helmet>
+      <div className="min-h-screen bg-white font-sans">
+        <div className="container mx-auto max-w-4xl px-4 py-8">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
