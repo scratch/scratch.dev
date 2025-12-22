ed716798cfb006d7eda613e4fd3ffd3c31e429fa
Pete Koomen <koomen@gmail.com>
2025-12-22 00:00:25 -0800

    remove old website files

--- website/components/h1.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function h1({ children }: { children: React.ReactNode }) {
-  return <h1 className="text-4xl font-bold mb-8">{children}</h1>;
-}
--- website/components/pre.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function pre({ children }: { children: React.ReactNode }) {
-  return (
-    <pre className="bg-gray-100 p-4 rounded-md overflow-x-auto my-4 font-mono">
-      {children}
-    </pre>
-  );
-}
